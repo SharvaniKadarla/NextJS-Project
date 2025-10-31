d1ab9ac2cfafa6313a3959f58d571fde98908421
Janvi Patel <janvipatel3052@gmail.com>
2025-10-30 21:47:39 -0400

    Completed CH 9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -19,11 +19,11 @@ export async function fetchRevenue() {
 
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve,3000));
-    const data = await sql<Revenue[]>`SELECT * FROM revenue`;
+    const result = await sql<Revenue[]>`SELECT * FROM revenue`;
 
     console.log('Data fetch completed after 3 seconds.');
 
-    return data;
+    return result;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
